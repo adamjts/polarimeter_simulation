4c25f4f026aa18a465c9de158351694cc85f9bbe
Adam Theriault-Shay <adamtheriault-shay@dhcp-18-189-57-122.dyn.mit.edu>
2016-06-08 16:11:02 -0400

    fixed zoom issue

--- componentGroups.py
+++ componentGroups.py
@@ -10,6 +10,7 @@ from marxs.optics.baffle import Baffle
 from marxs.optics.multiLayerMirror import MultiLayerMirror
 from marxs.optics.grating import FlatGrating
 from marxs.optics.detector import FlatDetector
+from marxs.optics import OpticalElement
 from marxs.optics.polarization import polarization_vectors
 
 from energyDistributions import createEnergyTable
@@ -17,7 +18,7 @@ from energyDistributions import createEnergyTable
 
 
 
-class SourceMLMirror():
+class SourceMLMirror(OpticalElement):
 	#this will also alllow the elements to wiggle later. RN it goes light source --> MLMirror
 	# The mirror is at the center of this object
 
@@ -45,13 +46,16 @@ class SourceMLMirror():
 
 		self.defaultMirrorPosition = np.array([0,0,0])
 
-		self.defaultMirrorPos4d = compose(self.defaultMirrorPosition, self.defaultMirrorOrientation, np.ones(3), np.zeros(3))
+		self.defaultMirrorPos4d = compose(self.defaultMirrorPosition, self.defaultMirrorOrientation, np.array([1, 24.5, 12]), np.zeros(3))
 
 		mirror = MultiLayerMirror(self.reflFile, self.testedPolarization,
-        pos4d = self.defaultMirrorPos4d) ; return mirror
+        pos4d = self.defaultMirrorPos4d); return mirror
 
 
-	def __init__(self, reflFile, testedPolarization, openningAngle, sourceDistance = 500, **kwargs):
+	def __init__(self, reflFile, testedPolarization, openningAngle = 0.05, sourceDistance = 500, **kwargs):
+		super(SourceMLMirror, self).__init__(**kwargs)
+		self.defaultApparatusPos4d = self.pos4d
+
 		# Generate Default Mirror
 		self.mirror = self.defaultMirror(reflFile, testedPolarization)
 		self.source = self.defaultSource(openningAngle, sourceDistance)
@@ -63,6 +67,10 @@ class SourceMLMirror():
 		   	|
 		   	|
 		   	MLMirror------>
+
+		   	self.pos4d is a record of all the transformations on the ENTIRE setup as a whole.
+		   	The source comes in by default from the local y direction.
+		   	The photons exit by default in the local x direction
 		'''
 	
 	def updateSource(self, position=None, openningAngle = None, direction = None):
@@ -85,7 +93,13 @@ class SourceMLMirror():
 
 
 	def __str__(self):
-		report = "CURRENT SETUP\n"
+
+		report = "APPARATUS GEOMETRY ****************** (Global Coordinates) \n"
+		report += "    -transformation record: \n" + str(self.pos4d)
+
+
+		report += "\n \n"
+		report += "CURRENT SETUP ****************** (Local Coordinates)\n"
 
 		report += "Mirror:\n" 
 
@@ -105,17 +119,11 @@ class SourceMLMirror():
 			
 
 	def generate_photons(self, exposureTime):
-
 		# Generate Initial Photons
 		photons = self.source.generate_photons(exposureTime)
-
-
-		reflectedPhotons = self.mirror.process_photons(photons)
-
-
-
-		# Removing photons with zero probability
+		reflectedPhotons = self.mirror.process_photons(photons)# Removing photons with zero probability 
 		rowsToRemove = []
+
 		for i in range(0,len(reflectedPhotons)):
 			if (reflectedPhotons[i]['probability']==0):
 				rowsToRemove.append(i)
@@ -123,9 +131,14 @@ class SourceMLMirror():
 		rowsToRemove = np.array(rowsToRemove)
 		reflectedPhotons.remove_rows(rowsToRemove)
 
-		#LATER DEPENDING ON THE ORIENTATION OF THE ENTIRE THING WE CAN ROTATE THEN ENTIRE APPRATUS (ROTATE ALL REFLECTED PHOTONS)
-
 		return reflectedPhotons
+
+
+
+
+		#Transform the reflected photons from the local coordinate system to the global coordinate system
+		#reflectedPhotons['dir'] = np.dot(self.pos4d, reflectedPhotons['dir'].T).T
+
 
 	def offset_mirror(self, offsetMatrix):
 		# This is how we offset the mirror relative to its default position
@@ -156,56 +169,9 @@ class SourceMLMirror():
 
 		self.updateSource(position = position)
 
+	def offset_apparatus(self,offsetMatrix):
+		self.pos4d = np.dot(offsetMatrix, self.defaultApparatusPos4d)
 
-
-
-'''
-
-	def move_mirror_orientation(self,rotationMatrix):
-
-		# Update Mirror Orientation
-		self.currentMirrorOrientation = np.dot(rotationMatrix, self.currentMirrorOrientation)
-
-		self.updateMirror()
-
-	def move_mirror_position(self, displacement):
-		# This is used to move the mirror relative to its current position
-
-		self.currentMirrorPosition = self.currentMirrorPosition + np.array(displacement)
-
-		self.updateMirror()
-
-
-
-'''
-
-
-
-'''
-	def offset_mirror_orientation(self, rotationMatrix):
-		# This is used to rotate the mirror relative to its default orientation
-
-		# Update Mirror Orientation
-		rotationMatrix = np.array(rotationMatrix)
-		self.currentMirrorOrientation = np.dot(rotationMatrix, self.defaultMirrorOrientation)
-
-		# Reset Mirror Position
-		self.currentMirrorPosition = self.defaultMirrorPosition
-
-		self.updateMirror()
-
-	def offset_mirror_position(self, position):
-		# This is used to place the mirror relative to its default position.
-
-		# Reset Mirror Orientation
-		self.currentMirrorOrientation = self.defaultMirrorOrientation
-
-		# Update Mirror Position
-		position = np.array(position)
-		self.currentMirrorPosition = position
-
-		self.updateMirror()
-		'''
 
 
 
